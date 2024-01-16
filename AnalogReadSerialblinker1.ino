bb3924922cbceb19e5af22495a3df606a57da37a
Zeckoth <156819190+Zeckoth@users.noreply.github.com>
2024-01-16 20:18:36 +0530

    Add files via upload

---
+++ AnalogReadSerialblinker1.ino
@@ -0,0 +1,42 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+*/
+int ledpin = 7;
+int brightness = 0;  // how bright the LED is
+int fadeAmount = 5; 
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+ 
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(10000);  // delay in between reads for stability
+  analogWrite(ledpin, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(sensorValue);
+}
